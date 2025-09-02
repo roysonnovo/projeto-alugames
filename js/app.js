e9c26d6a8ae4ef54a34c2cdbb9c5c42a1dc4bb02
roysonnovo <royson.novo@gmail.com>
2025-09-02 09:44:24 -0300

    Alugar e Devolver

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 function alterarStatus(id){
-    let  gameClick = document.getElementById(`game-${id}`);
+    let gameClick = document.getElementById(`game-${id}`);
     let img = gameClick.querySelector('.dashboard__item__img');
     let botao = gameClick.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClick.querySelector('.dashboard__item__name');
 
-    alert(nomeJogo.textContent);
+    if (img.classList.contains('dashboard__item__img--rented')){
+        img.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        img.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+    }
 }
