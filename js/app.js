6cbcbdb56bc419288130fe11ac4a4cf5abb12d65
roysonnovo <royson.novo@gmail.com>
2025-09-02 09:50:52 -0300

    Confirmação Devolver Jogo

--- js/app.js
+++ js/app.js
@@ -2,11 +2,15 @@ function alterarStatus(id){
     let gameClick = document.getElementById(`game-${id}`);
     let img = gameClick.querySelector('.dashboard__item__img');
     let botao = gameClick.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClick.querySelector('.dashboard__item__name');
 
     if (img.classList.contains('dashboard__item__img--rented')){
-        img.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
+        // Adiciona uma confirmação antes de devolver o jogo
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            img.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }
     } else {
         img.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver';
